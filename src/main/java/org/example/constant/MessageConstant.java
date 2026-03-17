5339f0138510bc1cc953aba8a525036289fd372f
PHUNG THANH DAT <phungthanhdat722006@gmail.com>
2026-03-17 21:40:57 +0700

    refactor: update message constants

--- src/main/java/org/example/constant/MessageConstant.java
+++ src/main/java/org/example/constant/MessageConstant.java
@@ -9,6 +9,13 @@ public class MessageConstant {
     public static final String REGISTER_USERNAME_EXIST = "Tên đăng nhập đã tồn tại!";
     public static final String REGISTER_FAIL = "Lỗi hệ thống: Đăng ký thất bại.";
 
+    public static final String FORGOT_PASS_EMAIL_NOT_EXIST = "Email này chưa được đăng ký trong hệ thống!";
+    public static final String FORGOT_PASS_SEND_FAIL = "Gửi mã thất bại. Vui lòng kiểm tra kết nối mạng!";
+    public static final String OTP_INVALID = "Mã OTP không chính xác.";
+    public static final String OTP_EXPIRED = "Mã OTP đã hết hạn. Vui Lòng gửi lại!";
+
+    public static final String UPDATE_FAIL = "Lỗi hệ thống: Không thể lưu thông tin. Vui lòng thử lại!";
+
     public static final String ERR_DB_CONNECT = "Lỗi kết nối CSDL, vui lòng thử lại!";
 
     public static final String ACCOUNT_LOCKED = "Tài khoản của bạn đã bị khóa!";
@@ -17,4 +24,5 @@ public class MessageConstant {
     public static final String ERR_DB_SAVE = "Không thể lưu dữ liệu vào hệ thống.";
     public static final String ERR_DB_DELETE = "Không thể xóa dữ liệu.";
     public static final String ERR_DB_UPDATE = "Cập nhật dữ liệu thất bại.";
+    public static final String ERR_USER_NOT_FOUND = "Không tìm thấy thông tin người dùng.";
 }
